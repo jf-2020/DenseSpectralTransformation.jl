d1e6203fd32e39f37afe9e1a9ee366c52bf9930e
Michael Stewart <mastewart@mastewart.org>
2025-08-21 13:47:02 -0400

    Reorganized tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using SafeTestsets
 using LinearAlgebra
 using Test
 
-@safetestset "LQDS Tests Lower, Float64" begin
+@safetestset "LQDS Tests" begin
     using LinearAlgebra
     using DenseSpectralTransformation
     include("../test/test_lqds.jl")
@@ -14,55 +14,31 @@ using Test
         for i = 1:n, j = 1:n
             ]
     tol = 1e-13
-    run_lqds_test(Hermitian(A, :L), tol)
-end
+    @testset "LQDS Tests Lower, Float64" begin
+        run_lqds_test(Hermitian(A, :L), tol)
+    end
+    @testset "LQDS Tests Upper, Float64" begin
+        run_lqds_test(Hermitian(A, :U), tol)
+    end
 
-@safetestset "LQDS Tests Upper, Float64" begin
-    using LinearAlgebra
-    using DenseSpectralTransformation
-    include("../test/test_lqds.jl")
-    n = 300
-    x = exp.(-.2 * (1:n))
-    A = Float64[
-        (i == j ? (-1)^i * x[abs(i - j) + 1] : x[abs(i - j) + 1])
-        for i = 1:n, j = 1:n
-            ]
-    tol = 1e-13
-    run_lqds_test(Hermitian(A, :U), tol)
-end
-
-@safetestset "LQDS Tests Lower, Complex{Float64}" begin
-    using LinearAlgebra
-    using DenseSpectralTransformation
-    include("../test/test_lqds.jl")
-    n = 300
     x = exp.(-.2 * (1:n))
     y = exp.(-.25 * (n:-1:1))
     A = Complex{Float64}[
         i == j ? (-1)^i * x[abs(i - j) + 1] : x[abs(i - j) + 1] + im*y[abs(i-j)+1]
         for i = 1:n, j = 1:n
             ]
-    tol = 1e-13
-    run_lqds_test(Hermitian(A, :L), tol)
+
+    @testset "LQDS Tests Lower, Complex{Float64}" begin
+        run_lqds_test(Hermitian(A, :L), tol)
+    end
+
+    @testset "LQDS Tests Upper, Complex{Float64}" begin
+        run_lqds_test(Hermitian(A, :L), tol)
+    end
+    
 end
 
-@safetestset "LQDS Tests Upper, Complex{Float64}" begin
-    using LinearAlgebra
-    using DenseSpectralTransformation
-    include("../test/test_lqds.jl")
-    n = 300
-    x = exp.(-.2 * (1:n))
-    y = exp.(-.25 * (n:-1:1))
-    A = Complex{Float64}[
-        i == j ? (-1)^i * x[abs(i - j) + 1] : x[abs(i - j) + 1] + im*y[abs(i-j)+1]
-        for i = 1:n, j = 1:n
-            ]
-    tol = 1e-13
-    run_lqds_test(Hermitian(A, :U), tol)
-end
-
-
-@safetestset "Eigenvalue/vector Test, Float64" begin
+@safetestset "Eigen Test" begin
     using LinearAlgebra
     using DenseSpectralTransformation
     include("../test/test_spectral.jl")
@@ -74,14 +50,11 @@ end
             ]
     B = [1/(i+j-1) for i in 1:n, j in 1:n]
     tol = 1e-13
-    run_spectral_test(Hermitian(A), Hermitian(B), 5.0, tol)
-end
 
-@safetestset "Eigenvalue/vector Test, Complex{Float64}" begin
-    using LinearAlgebra
-    using DenseSpectralTransformation
-    include("../test/test_spectral.jl")
-    n = 300
+    @testset "Eigen Test, Float64" begin
+        run_spectral_test(Hermitian(A), Hermitian(B), 5.0, tol)
+    end
+
     x = exp.(-.2 * (1:n))
     y = exp.(-.25 * (n:-1:1))
     A = zeros(Complex{Float64}, n, n)
@@ -94,6 +67,8 @@ end
     end
     A=A+A'
     B = Complex{Float64}[1/(i+j-1) for i in 1:n, j in 1:n]
-    tol = 1e-13
-    run_spectral_test(Hermitian(A), Hermitian(B), 5.0, tol)
+
+    @testset "Eigen Test, Complex{Float64}" begin
+        run_spectral_test(Hermitian(A), Hermitian(B), 5.0, tol)
+    end
 end
